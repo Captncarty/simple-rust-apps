5cf899192b1135fc01a5c97416d96af9e35fd47e
kelvin Maccarthy <edithmukoro3@gmail.com>
2024-08-27 00:50:11 +0100

    updating the application to draw an image

---
+++ desktop-application/README.md
@@ -0,0 +1,18 @@
+<h1 align="center">RUST DESKTOP APPLICATION</h1>
+
+
+<h1 align="center">This is a simple project focused on building a simple desktop application that can be used to make drawings at the click of a button</h1>
+<p align ="center">
+    <img width="500" height="300" src="src/img/cli.jpg">
+</p>
+
+---------
+
+## Getting Started
+
+Clone this repository and run the script `./sys_config`
+
+Run the code using `cargo run -q`
+
+### AUTHOR
+*Kelvin Maccarthy* - [@Captncarty](https://github.com/Captncarty)
--- desktop-application/src/main.rs
+++ desktop-application/src/main.rs
@@ -1,15 +1,37 @@
+// Imports
+use ansi_term::Colour;
+use std::thread;
+use std::time::Duration;
+
 /// Designing the UI
 use druid::widget::{Label, Button, Flex};
 use druid::{AppLauncher, WindowDesc, Widget, PlatformError};
 use druid::WidgetExt;
 
+
+//Drawing a Ferris
+fn draw() {
+    let ferris: String = Colour::Red.paint(r"
+    .
+     .
+      .
+       █ █           █ █
+        ▀█  ▄█████▄  █▀
+         ▀▄███▀█▀███▄▀
+         ▄▀███▀▀▀███▀▄
+         █ ▄▀▀▀▀▀▀▀▄ █
+    ").to_string();
+    println!("{}", ferris);
+}
+
 /// Function to build UI using druid's widgets
 fn build_ui() -> impl Widget<()> {
     let mut col = Flex::column();
 
-    col.add_child(Label::new("Hello, Rust World!").center());
-    col.add_child(Button::new("Click me!").on_click(|_ctx, _data, _env| {
-        println!("Button clicked!");
+    col.add_child(Label::new("Draw a Ferris!").center());
+    col.add_child(Button::new("Click to Draw!").on_click(|_ctx, _data, _env| {
+        draw();
+        thread::sleep(Duration::from_millis(2000));
     }).center());
 
     col
@@ -18,7 +40,7 @@ fn build_ui() -> impl Widget<()> {
 fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(build_ui())
         .title("A simple Rust Desktop App")
-        .window_size((400.0, 400.0));
+        .window_size((800.0, 400.0));
 
     AppLauncher::with_window(main_window)
         .launch(())?;
